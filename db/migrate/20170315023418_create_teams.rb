fe2fc4017ae5542a140af483bbe6eaba950b6b63
Laurence Smith <laurence@sourceless.org>
2017-03-15 02:36:33 +0000

    Add teams

---
+++ db/migrate/20170315023418_create_teams.rb
@@ -0,0 +1,15 @@
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.references :account, foreign_key: true
+      t.integer :manager_id
+
+      t.timestamps
+    end
+
+    add_index :teams, :manager_id
+
+    add_foreign_key :users, column: :manager_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315022711) do
+ActiveRecord::Schema.define(version: 20170315023418) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20170315022711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_accounts_on_owner_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "account_id"
+    t.integer  "manager_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_teams_on_account_id"
+    t.index ["manager_id"], name: "index_teams_on_manager_id"
   end
 
   create_table "users", force: :cascade do |t|
