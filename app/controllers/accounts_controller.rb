459c34b96bffa65098729a0b3c01a97d8b60b13b
Laurence Smith <laurence@sourceless.org>
2017-03-15 02:32:08 +0000

    Scaffold out account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,23 @@
 class AccountsController < ApplicationController
+  def index
+    # Redirect to new if current user doesn't have an account,
+    # or to show if they do
+  end
+  
+  def new
+    # Create an account
+  end
+
+  def create
+  end
+
+  def show
+    # Show account if user has one
+  end
+
+  def edit
+    # Edit account options
+  end
+
+  # No update or delete for now
 end
