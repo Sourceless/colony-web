b465e9ecc32b21598ed82c4472677457342f9fc6
Laurence Smith <laurence@sourceless.org>
2017-03-28 23:55:16 +0100

    Make dash accessible

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  resources :teams
-  resources :accounts
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :accounts
   resources :teams
 
   get '/pricing', to: 'pages#pricing'
   get '/product', to: 'pages#product'
+
+  get '/dashboard', to: 'dashboard#dashboard'
 
   root to: 'pages#home'
 end
