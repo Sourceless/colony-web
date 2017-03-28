57a5e5a77890212b1b24974d89740b2ba3aa96ac
Laurence Smith <laurence@sourceless.org>
2017-03-28 16:37:48 +0100

    Fix lock issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+append :linked_files, "config/database.yml"
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
