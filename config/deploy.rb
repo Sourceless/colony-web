9a8457e94e32e446a36312c2c121d446b670a57e
Laurence Smith <laurence@sourceless.org>
2017-03-28 17:22:36 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 append :linked_files, "config/database.yml"
+append :linked_files, "config/nginx.conf"
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
