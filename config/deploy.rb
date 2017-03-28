9d00987aaa7eb9d59db011fc0474524d1e61d2ff
Laurence Smith <laurence@sourceless.org>
2017-03-28 03:57:40 +0100

    Fix puma for deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-# config valid only for current version of Capistrano
-lock "3.8.0"
-
 set :application, "colony"
 set :repo_url, "git@github.com:Sourceless/colony-web.git"
 set :user,            'deploy' # TODO: change this!!
